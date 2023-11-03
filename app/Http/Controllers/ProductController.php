7a8685a4917a8f8084088617ce1a675be3d6f763
Aditya Surana <suranaaditya3@gmail.com>
2023-11-03 09:39:04 +0530

    initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    //
+    public function index(){
+        return view('products.index');
+    }
+    public function create(){
+        return view('products.create');
+    }
+
+    public function store(Request $request){
+        // dd($request->all());
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'image'=> 'required|mimes:jpeg, jpg, png,gif|max:10000'
+        ]);
+        $imageName = time().'.'.$request->image->extension();
+        $request->image->move(public_path('products'), $imageName);
+
+        $product = new Product;
+        $product->image = $imageName;
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->save();
+        return back();
+
+    }
+}
